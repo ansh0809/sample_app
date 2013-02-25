782e22f085908322faa8402b36aafdf73f684069
Anshuman Khemka <anshumankhemka@hotmail.com>
2013-02-25 23:29:59 +0530

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Anshuman Khemka"
+    email    "anshumankhemka@hotmail.com"
+    password "anshu123"
+    password_confirmation "anshu123"
+  end
+end
